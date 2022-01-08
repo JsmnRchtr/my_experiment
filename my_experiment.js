ddf750552f34f4a88f0ae303da5df77a51053218
JXRichter <jasmin.richter@psykologi.uio.no>
2022-01-08 17:47:26 +0100

    set-up rt task in js

--- my_experiment.js
+++ my_experiment.js
@@ -1,10 +1,95 @@
-const jsPsych = initJsPsych(); //initialize jspsych
-    
-// define trial
-const hello_trial = {
+var jsPsych = initJsPsych({
+  on_finish: function() {
+  jsPsych.data.displayData();
+  }
+}); //initialize jspsych
+
+var timeline = [];
+
+var preload = {
+  type: jsPsychPreload,
+  images: ['img/blue.png', 'img/orange.png']
+};
+
+timeline.push(preload);
+
+var welcome = {
   type: jsPsychHtmlKeyboardResponse,
-  stimulus: 'Hello world!'
-}
+  stimulus: "Welcome to the experiment. Press any key to start."
+};
+timeline.push(welcome);
+
+var instruction = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: `<p>In this experiment, a circle will appear in the center 
+    of the screen.</p><p>If the circle is <strong>blue</strong>, 
+    press the letter F on the keyboard as fast as you can.</p>
+    <p>If the circle is <strong>orange</strong>, press the letter J 
+    as fast as you can.</p>
+    <div style='width: 700px;'>
+    <div style='float: left;'><img src='img/blue.png'></img>
+    <p class='small'><strong>Press the F key</strong></p></div>
+    <div style='float: right;'><img src='img/orange.png'></img>
+    <p class='small'><strong>Press the J key</strong></p></div>
+    </div>
+    <p>Press any key to begin.</p>`,
+  post_trial_gap: 2000
+};
+timeline.push(instruction);
+
+var test_stimuli = [
+  { stimulus: "img/blue.png",  correct_response: 'f'},
+  { stimulus: "img/orange.png",  correct_response: 'j'}
+];
+
+var fixation = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: '<div style="font-size:60px;">+</div>',
+  choices: "NO_KEYS",
+  trial_duration: function(){
+    return jsPsych.randomization.sampleWithoutReplacement([250, 500, 750, 1000, 1250, 1500, 1750, 2000], 1)[0];
+  },
+  data: {
+    task: 'fixation'
+  }
+};
+
+var test_trial = {
+  type: jsPsychImageKeyboardResponse,
+  stimulus: jsPsych.timelineVariable('stimulus'),
+  choices: ['f', 'j'],
+  data: {
+    task: 'response',
+    correct_response: jsPsych.timelineVariable('correct_response')
+  },
+  on_finish: function(data){
+    data.correct = jsPsych.pluginAPI.compareKeys(data.response, data.correct_response);
+  }
+};
+
+//run the test_procedure timeline one time for each entry in the test_stimuli array
+var test_procedure = {
+  timeline: [fixation, test_trial],
+  timeline_variables: test_stimuli,
+  randomize_order: true,
+  repetitions: 2
+};
+timeline.push(test_procedure);
+
+var debrief_block = {
+ type: jsPsychHtmlKeyboardResponse,
+ stimulus: function() {
+  var trials = jsPsych.data.get().filter({task: 'response'});
+  var correct_trials = trials.filter({correct: true});
+  var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
+  var rt = Math.round(correct_trials.select('rt').mean());
+
+  return `<p>You responded correctly on ${accuracy} % of the trials.</p>
+  <p>Your average response time was ${rt} ms.</p>
+  <p>Press any key to complete the experiment. Thank you!</p>`;
+  }
+};
+timeline.push(debrief_block);
 
 // run timeline
-jsPsych.run([hello_trial]);
+jsPsych.run(timeline);
